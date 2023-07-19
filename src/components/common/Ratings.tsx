21fc39b0dd8d0c61c6a79e012875453aec19dfa5
phemi <mrlapinho@gmail.com>
2023-07-19 22:17:14 +0100

    Add missing key prop to ratings component

--- src/components/common/Ratings.tsx
+++ src/components/common/Ratings.tsx
@@ -14,6 +14,7 @@ const Rating = ({ rating }: RatingProps) => {
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
                     className="mt-2"
+                    key={index}
                 >
                     <path
                         fillRule="evenodd"
